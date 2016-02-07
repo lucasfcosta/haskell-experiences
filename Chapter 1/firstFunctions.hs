01f36c3b94fa1d4456568497cecf5499122b86aa
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-07 14:30:41 -0200

    Add example about infix functions for firstFunctions chapter

--- Chapter 1/firstFunctions.hs
+++ Chapter 1/firstFunctions.hs
@@ -6,6 +6,9 @@ doubleMe x = x + x
 -- Calling a function from another function
 doubleAndSum x y = doubleMe x + doubleMe y
 
+-- We can call them as infix functions using backticks on the functions' name
+five = 10 `div` 2
+
 -- Flow control
 -- Details: In Haskell the `if`s are expressions, this means they always return a value.
 -- 			Due to this every `if` statement requires an `else`.
